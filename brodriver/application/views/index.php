f4e2aa466925a0b9dcfe998bf898c0680ec35504
pedr0369 <pedr0369@hotmail.com>
2016-03-14 18:32:10 -0300

    agoravai

--- brodriver/application/views/index.php
+++ brodriver/application/views/index.php
@@ -4,9 +4,9 @@
     <title>Bootstrap Admin Theme v3</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="<?= base_url('css/bootstrap/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('bootstrap/bootstrap.min.css'); ?>" rel="stylesheet">
     <!-- styles -->
-    <link href="<?= base_url('css/bootstrap/styles.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('bootstrap/styles.css'); ?>" rel="stylesheet">
 
   </head>
   <body>
@@ -238,7 +238,7 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="js/custom.js"></script>
+    <script src="<?= base_url('bootstrap/bootstrap.min.js'; ?>"></script>
+    <script src="<?= base_url('js/custom.js'; ?>"></script>
   </body>
 </html>
